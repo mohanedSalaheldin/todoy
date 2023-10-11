fcbfdbc9841ecca2f8c87bfca129d734cc291127
mohanedSalaheldin <mohnsd1162002@gmail.com>
2023-10-11 18:06:50 +0300

    display task for Firebase

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -1,11 +1,55 @@
 import 'package:flutter/material.dart';
-
+import 'package:todo_with_firebase_08oct/shered/components/components.dart';
+import 'package:todo_with_firebase_08oct/shered/network/romote/firebase_helper.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Notes'),);
+    return Center(
+      child: StreamBuilder(
+        stream: FirebaseHelper.getTasks(),
+        builder: (context, snapshot) {
+          // if (snapshot.hasError) {
+          // return const Center(
+          //   child: Icon(Icons.signal_wifi_connected_no_internet_4_outlined),
+          // );
+          // }
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.connectionState == ConnectionState.none) {
+            return const Center(
+              child: Icon(
+                Icons.signal_wifi_connected_no_internet_4_outlined,
+                size: 30.0,
+              ),
+            );
+          }
+          return ListView.separated(
+            itemBuilder: (context, index) {
+              var model = snapshot.data!.docs[index].data();
+              return buildTask(
+                context: context,
+                taskDesc: model['taskDesc'],
+                taskName: model['taskName'],
+                tasktag: model['taskTag'],
+              );
+            },
+            separatorBuilder: (context, index) {
+              return const SizedBox(
+                height: 10.0,
+              );
+            },
+            itemCount: snapshot.data!.docs.length,
+          );
+        },
+      ),
+      //  buildTask(context: context),
+    );
   }
 }
--- lib/shered/styles/colors.dart
+++ lib/shered/styles/colors.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 Color defaultColor = const Color.fromARGB(255, 52, 100, 128);
+Color appBarDefaultColor = const Color.fromARGB(255, 242, 244, 245);
